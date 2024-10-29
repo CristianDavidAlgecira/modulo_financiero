76b0bbacee2a8d71dfac72feffcc2b51ab9fe512
Juan Pablo Suarez Ramirez <jpsuarez40@gmail.com>
2024-10-29 12:18:19 -0500

    feature/login

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  @Input() user: any = [];
+  menuVisible = false;
+
+  constructor(private authService: AuthService) {
+  }
+
+  onFocus() {
+    this.menuVisible = true;
+  }
+
+  onFocusOut() {
+    setTimeout(() => {
+      this.menuVisible = false;
+    }, 100);
+  }
+
+  logout() {
+    console.log("cerrar sesion");
+    this.authService.logout();
+  }
 
 }

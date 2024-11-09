d09fd773c4ecb25f84683b7033ff809c5583787a
Juan Pablo Suarez Ramirez <jpsuarez40@gmail.com>
2024-11-09 09:05:18 -0500

    feature/apiDatosMaestros

---
+++ src/app/services/api/api.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl = environment.MAESTROS;
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  //Tipo requerimiento (nombre req)
+  getTipoRequerimiento(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/catalogos/MFTipoRequerimiento`);
+  }
+
+  //Periodo de entrega
+  getPeriodoEntrega(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/catalogos/MFPeriodoEntrega`);
+  }
+
+  //tipo programacion
+  getTipoProgramacion(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/catalogos/MFTipoProgramacion`);
+  }
+
+  //Estado vigilado
+  getEstadoVigilado(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/catalogos/MFEstadoVigilado`);
+  }
+
+  //Delegaturas
+  getDelegaturas(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/catalogos/Delegaturas`);
+  }
+
+  //TIPO VIGILADO
+  getTipoVigilado(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/catalogos/MFVigilados`);
+  }
+
+  //Numeros digito NIT
+  getTipoDigitoNIT(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/catalogos/MFProgramacionDigitos`);
+  }
+
+}

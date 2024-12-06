35c9b8d1a6f475994015549970765ee7b8bd9640
Cristian Algecira <cristian.algecira.99@gamil.com>
2024-12-06 12:29:37 -0500

    Cambio 53

--- src/app/vistas/spring-2/formulario-requerimiento-anulacion/formulario-requerimiento-anulacion.component.ts
+++ src/app/vistas/spring-2/formulario-requerimiento-anulacion/formulario-requerimiento-anulacion.component.ts
@@ -3,6 +3,8 @@ import {PrimaryButtonComponent} from "../../../componentes/primary-button/primar
 import {Router} from "@angular/router";
 import {PaginatorModule} from "primeng/paginator";
 import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {FileUploadComponent} from "../../../componentes/file-upload/file-upload.component";
+import {AlertComponent} from "../../../componentes/alert/alert.component";
 
 @Component({
   selector: 'app-formulario-requerimiento-anulacion',
@@ -12,7 +14,9 @@ import {NgClass, NgForOf, NgIf} from "@angular/common";
     PaginatorModule,
     NgClass,
     NgForOf,
-    NgIf
+    NgIf,
+    FileUploadComponent,
+    AlertComponent
   ],
   templateUrl: './formulario-requerimiento-anulacion.component.html',
   styleUrl: './formulario-requerimiento-anulacion.component.css'
@@ -22,8 +26,74 @@ export class FormularioRequerimientoAnulacionComponent implements OnInit {
   nombreVigilado: string = '';
   tipoIdentifiacionVigilado: string = '';
   titularTramite: string = '';
+  nombres: string = '';
+  primerApellido: string = '';
+  segundoApellido: string = '';
+  numeroIdentificacion: string = '';
+  medioRespuesta: string = '';
+  email: string = '';
+  emailConfirma: string = '';
+  emailAlternativo: string = '';
+  direccion: string = '';
+  telefono: string = '';
+  paises: string = '';
+  departamentos: string = '';
+  municipios: string = '';
+  tipoPQR: string = '';
+  tipoSolicitud: string = '';
+  showModalInicial: boolean = false;
+
+  // Propiedades del input: tamaño, info, etc.
+  dataClass = {
+    textSize: 'xs', textInfo: 'Archivo PDF',
+  };
+
+  // Propiedad de objeto para manejar errores
+  errorStates: { [key: number]: boolean } = {};
 
   titularTramiteDatos = [
+    {
+      id: 1,
+      descripcion: ""
+    }
+  ]
+
+  medioRespuestaDatos = [
+    {
+      id: 1,
+      descripcion: ""
+    }
+  ]
+
+  paisesDatos = [
+    {
+      id: 1,
+      descripcion: ""
+    }
+  ]
+
+  departamentosDatos = [
+    {
+      id: 1,
+      descripcion: ""
+    }
+  ]
+
+  municipiosDatos = [
+    {
+      id: 1,
+      descripcion: ""
+    }
+  ]
+
+  tipoPQRDatos = [
+    {
+      id: 1,
+      descripcion: ""
+    }
+  ]
+
+  tipoSolicitudDatos = [
     {
       id: 1,
       descripcion: ""
@@ -33,6 +103,19 @@ export class FormularioRequerimientoAnulacionComponent implements OnInit {
   touchedFields = {
 
     titularTramite: false,
+    nombres: false,
+    primerApellido: false,
+    segundoApellido: false,
+    numeroIdentificacion: false,
+    medioRespuesta: false,
+    email: false,
+    emailConfirma: false,
+    telefono: false,
+    paises: false,
+    departamentos: false,
+    municipios: false,
+    tipoPQR: false,
+    tipoSolicitud: false
 
   };
 
@@ -42,6 +125,21 @@ export class FormularioRequerimientoAnulacionComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.showModalInicial = true;
+  }
+
+  preventClose(): void {
+    this.showModalInicial = true;
+  }
+
+  onAccept(): void {
+    console.log('Modal aceptado');
+    this.showModalInicial = false;
+  }
+
+  onDecline(): void {
+    console.log('Modal rechazado');
+    this.showModalInicial = false;
   }
 
   validateField(field: string): void {
@@ -54,6 +152,32 @@ export class FormularioRequerimientoAnulacionComponent implements OnInit {
   submitForm(): void {
 
     this.touchedFields.titularTramite = true;
+    this.touchedFields.nombres = true;
+    this.touchedFields.primerApellido = true;
+    this.touchedFields.segundoApellido = true;
+    this.touchedFields.numeroIdentificacion = true;
+    this.touchedFields.medioRespuesta = true;
+    this.touchedFields.email = true;
+    this.touchedFields.telefono = true;
+    this.touchedFields.paises = true;
+    this.touchedFields.departamentos = true;
+    this.touchedFields.municipios = true;
+    this.touchedFields.tipoPQR = true;
+    this.touchedFields.tipoSolicitud = true;
+
+  }
+
+  OnUploadButton(file: File[]) {
+
+    if (file[0]) {
+
+      console.log('Archivo Cargado', file[0]);
+
+    } else {
+
+      console.log('Archivo con error');
+
+    }
 
   }
 

c53d8e26f2b0207ccc908bea535eb1bc1a0fce49
Juan Pablo Suarez Ramirez <jpsuarez40@gmail.com>
2024-12-11 22:29:47 -0500

    feature/jsonCeldas-dictamen

--- src/app/vistas/spring-2/visualizar-archivo/visualizar-archivo.component.ts
+++ src/app/vistas/spring-2/visualizar-archivo/visualizar-archivo.component.ts
@@ -8,7 +8,8 @@ import {AlertComponent} from "../../../componentes/alert/alert.component";
 
 @Component({
   selector: 'app-visualizar-archivo',
-  standalone: true, imports: [FileUploadComponent, PrimaryButtonComponent, AlertComponent],
+  standalone: true,
+  imports: [FileUploadComponent, PrimaryButtonComponent, AlertComponent],
   templateUrl: './visualizar-archivo.component.html',
   styleUrl: './visualizar-archivo.component.css'
 })
@@ -16,7 +17,7 @@ import {AlertComponent} from "../../../componentes/alert/alert.component";
 export class VisualizarArchivoComponent implements OnInit {
 
   // Propiedad de objeto para manejar errores
-  errorStates: { [key: number]: boolean } = {};
+  errorStates: {[key: number]: boolean} = {};
 
   // Propiedades del input: tamaño, info, etc.
   dataClass = {
@@ -39,70 +40,43 @@ export class VisualizarArchivoComponent implements OnInit {
 
   // Ejemplo de validationRanges
   validationRanges = {
-    "validationRanges": [
-      {
-        "sheetName": "INDICE",
-        "keywords": {}
-      },
-      {
-        "sheetName": "Identificación del Vigilado",
-        "keywords": {
-          "DATOS BÁSICOS": 1
-        }
-      },
-      {
-        "sheetName": "ESF",
-        "keywords": {
-          "CORRECTO": 18
-        }
-      },
-      {
-        "sheetName": "ER",
-        "keywords": {
-          "CORRECTO": 2,
-          "ESTADO DE RESULTADOS": 1,
-        }
-      },
-      {
-        "sheetName": "ORI",
-        "keywords": {
-          "CORRECTO": 2,
-          "ESTADO DE RESULTADO INTEGRAL componentes ORI - OTRO RESULTADO INTEGRAL": 1,
-        }
-      },
-      {
-        "sheetName": "EFE-indirecto",
-        "keywords": {
-          "CORRECTO": 2,
-          "ESTADO DE FLUJO DE EFECTIVO (método indirecto)": 1,
-        }
-      },
-      {
-        "sheetName": "EFE-directo",
-        "keywords": {
-          "CORRECTO": 2,
-          "ESTADO DE FLUJO DE EFECTIVO(método directo)": 1,
-        }
-      },
-      {
-        "sheetName": "ECP",
-        "keywords": {
-          "CORRECTO": 4,
-          "ESTADO DE CAMBIOS EN EL PATRIMONIO": 1
-        }
-      },
-      {
-        "sheetName": "Dictamen",
-        "keywords": {
-          "CORRECTO": 1,
-          "DICTAMEN DEL REVISOR FISCAL": 1,
-        }
-      },
-      {
-        "sheetName": "Listas desplegables(ocultar)",
-        "keywords": {}
+    "validationRanges": [{
+      "sheetName": "INDICE", "keywords": {}
+    }, {
+      "sheetName": "Identificación del Vigilado", "keywords": {
+        "DATOS BÁSICOS": 1
       }
-    ]
+    }, {
+      "sheetName": "ESF", "keywords": {
+        "CORRECTO": 18
+      }
+    }, {
+      "sheetName": "ER", "keywords": {
+        "CORRECTO": 2, "ESTADO DE RESULTADOS": 1,
+      }
+    }, {
+      "sheetName": "ORI", "keywords": {
+        "CORRECTO": 2, "ESTADO DE RESULTADO INTEGRAL componentes ORI - OTRO RESULTADO INTEGRAL": 1,
+      }
+    }, {
+      "sheetName": "EFE-indirecto", "keywords": {
+        "CORRECTO": 2, "ESTADO DE FLUJO DE EFECTIVO (método indirecto)": 1,
+      }
+    }, {
+      "sheetName": "EFE-directo", "keywords": {
+        "CORRECTO": 2, "ESTADO DE FLUJO DE EFECTIVO(método directo)": 1,
+      }
+    }, {
+      "sheetName": "ECP", "keywords": {
+        "CORRECTO": 4, "ESTADO DE CAMBIOS EN EL PATRIMONIO": 1
+      }
+    }, {
+      "sheetName": "Dictamen", "keywords": {
+        "CORRECTO": 1, "DICTAMEN DEL REVISOR FISCAL": 1,
+      }
+    }, {
+      "sheetName": "Listas desplegables(ocultar)", "keywords": {}
+    }]
   };
   fieldgrupo1 = {
     "Identificación del Vigilado": {
@@ -131,9 +105,8 @@ export class VisualizarArchivoComponent implements OnInit {
       "periodicidadPresentacion": "F31",
       "anoActualReporte": "F32",
       "anoComparativo": "F33",
-      "nit": "NitValue"
-    },
-    "ESF": {
+
+    }, "ESF": {
       "totalActivosCorrientes": "H12",
       "efectivoYEquivalentesAlEfectivo": "H13",
       "efectivoRestringido": "H14",
@@ -213,8 +186,7 @@ export class VisualizarArchivoComponent implements OnInit {
       "totalPasivoPatrimonio": "H97",
       "validacionEcuacionPatrimonial": "H99",
       "nit": "NitValue"
-    },
-    "ORI": {
+    }, "ORI": {
       "gananciaPerdidaNeta": "J10",
       "otroResultadoIntegralDiferenciasCambioConversion": "J13",
       "otroResultadoIntegralGananciasActuariales": "J14",
@@ -229,18 +201,117 @@ export class VisualizarArchivoComponent implements OnInit {
       "resultadoIntegralParticipacionesNoControladoras": "J25",
       "validacionEstadoResultados": "J27",
       "nit": "NitValue"
+    }, "EFE-indirecto": {
+      "gananciaPerdida": "J10",
+      "ajustesGastosImpuestosGanancias": "J12",
+      "ajustesGastosDepreciacionAmortizacion": "J13",
+      "ajustesDeterioroReversionPerdidas": "J14",
+      "ajustesProvisiones": "J15",
+      "ajustesCostosFinancieros": "J16",
+      "ajustesPerdidasGananciasMonedaExtranjera": "J17",
+      "ajustesPerdidasGananciasValorRazonable": "J18",
+      "ajustesGananciasNoDistribuidasAsociadas": "J19",
+      "ajustesPerdidasGananciasDisposicionActivosNoCorrientes": "J20",
+      "otrosAjustesConciliarGananciaPerdida": "J21",
+      "totalAjustesConciliarGananciaPerdida": "J22",
+      "ajustesDisminucionesIncrementosInventarios": "J24",
+      "ajustesDisminucionIncrementoCuentasPorCobrar": "J25",
+      "ajustesDisminucionesIncrementosOtrasCuentasCobrar": "J26",
+      "ajustesIncrementoDisminucionCuentasPorPagar": "J27",
+      "ajustesIncrementosDisminucionesOtrasCuentasPagar": "J28",
+      "otrasEntradasSalidasEfectivo": "J29",
+      "flujosEfectivoNetosActividadesOperacion": "J30",
+      "flujosEfectivoPerdidaControlSubsidiarias": "J32",
+      "flujosEfectivoObtenerControlSubsidiarias": "J33",
+      "cobrosVentaPatrimonioInstrumentosDeuda": "J34",
+      "pagosAdquirirPatrimonioInstrumentosDeuda": "J35",
+      "cobrosVentaParticipacionesNegociosConjuntos": "J36",
+      "pagosAdquirirParticipacionesNegociosConjuntos": "J37",
+      "ventaPropiedadesPlantaEquipo": "J38",
+      "comprasPropiedadesPlantaEquipo": "J39",
+      "ventaActivosIntangibles": "J40",
+      "comprasActivosIntangibles": "J41",
+      "recursosVentasOtrosActivosLargoPlazo": "J42",
+      "comprasOtrosActivosLargoPlazo": "J43",
+      "anticiposPrestamosConcedidosTerceros": "J44",
+      "cobrosReembolsoAnticiposPrestamos": "J45",
+      "pagosContratosDerivados": "J46",
+      "cobrosContratosDerivados": "J47",
+      "dividendosRecibidos": "J48",
+      "interesesRecibidos": "J49",
+      "interesesPagados": "J51",
+      "flujosEfectivoNetosActividadesInversion": "J53",
+      "aumentosCapitalRecolocacionAcciones": "J55",
+      "disminucionCapitalReadquisicionAcciones": "J56",
+      "pagosOtrasParticipacionesPatrimonio": "J57",
+      "aumentoPrimaEmision": "J58",
+      "disminucionPrimaEmision": "J59",
+      "importesProcedentesPrestamos": "J60",
+      "reembolsosPrestamos": "J61",
+      "pagosPasivosArrendamientosFinancieros": "J62",
+      "dividendosPagados": "J63",
+      "flujosEfectivoNetosActividadesFinanciacion": "J66",
+      "incrementoDisminucionEfectivoEquivalentes": "J67",
+      "efectosTasaCambioEfectivoEquivalentes": "J69",
+      "efectivoEquivalentesPrincipioPeriodo": "J71",
+      "efectivoEquivalentesFinalPeriodo": "J72",
+      "nit": 'nit'
+
+    }, "EFE-directo": {
+      "flujosEfectivoProcedentesUtilizadosActividadesOperacion": "j9",
+      "clasesCobrosActividadesOperacion": "J10",
+      "cobrosVentasBienesPrestacionServicios": "J11",
+      "cobrosRegaliasCuotasComisionesOtrosIngresos": "J12",
+      "cobrosContratosIntermediacionNegociacion": "J13",
+      "cobrosPrimasPrestacionesAnualidadesPolizas": "J14",
+      "cobrosRentasVentasActivosMantenidosTerceros": "J15",
+      "otrosCobrosPorActividadesOperacion": "J16",
+      "clasesPagosEfectivoActividadesOperacion": "J17",
+      "pagosProveedoresSuministroBienesServicios": "J18",
+      "pagosContratosIntermediacionNegociacion": "J19",
+      "pagosCuentaEmpleados": "J20",
+      "pagosPrimasPrestacionesAnualidadesPolizas": "J21",
+      "pagosActivosMantenidosTercerosVenta": "J22",
+      "otrosPagosPorActividadesOperacion": "J23",
+      "flujosEfectivoNetosUtilizadosOperaciones": "J24",
+      "dividendosPagados": "J25",
+      "dividendosRecibidos": "J26",
+      "interesesPagados": "J27",
+      "interesesRecibidos": "J28",
+      "impuestosGananciasReembolsadosPagados": "J29",
+      "otrasEntradasSalidasEfectivo": "J30",
+      "flujosEfectivoNetosUtilizadosInversion": "J31",
+      "recursosCambioPropiedadSubsidiarias": "J57",
+      "pagosCambioPropiedadSubsidiarias": "J58",
+      "importesEmisionAcciones": "J59",
+      "importesEmisionInstrumentosPatrimonio": "J60",
+      "pagosAdquirirRescatarAccionesEntidad": "J61",
+      "pagosOtrasParticipacionesPatrimonio": "J62",
+      "importesPrestamos": "J63",
+      "reembolsosPrestamos": "J64",
+      "pagosPasivosArrendamientosFinancieros": "J65",
+      "importesSubvencionesGobierno": "J66",
+      "incrementoDisminucionNetoEfectivo": "J74",
+      "efectosVariacionTasaCambioEfectivo": "J76",
+      "efectivoEquivalentesPrincipioPeriodo": "J78",
+      "efectivoEquivalentesFinalPeriodo": "J79",
+      "nit": "nit"
+
+    }, "Dictamen": {
+      "dictamen": "F11",
+      "opinionDictamen": "F13",
+      "contenidoSalvedadDictamen": "F14",
+      "parrafoEnfasisDictamen": "F15",
+      "hipotesisNegocioMarcha": "F16",
+      "validacionHNM": "F17",
+      "nit": "nit",
+      "annio": "F9"
     }
+
   }
 
-
-
   // Constructor
-  constructor(
-    private errorService: ErrorService,
-    private ApiMFService: ApiMFService,
-    private router: Router,
-    private cdr: ChangeDetectorRef,
-  ) {
+  constructor(private errorService: ErrorService, private ApiMFService: ApiMFService, private router: Router, private cdr: ChangeDetectorRef,) {
   }
 
   ngOnInit() {
@@ -258,13 +329,13 @@ export class VisualizarArchivoComponent implements OnInit {
   }
 
   OnUploadButton(file: File[]) {
-    if (file[0]) {
+    if(file[0]) {
 
       this.showLoadingModal = true;
       this.ApiMFService.uploadFileAPI(file[0], this.validationRanges).subscribe({
         next: (response) => {
           this.showLoadingModal = false;
-          if(response.message == 'Archivo procesado y validado correctamente'){
+          if(response.message == 'Archivo procesado y validado correctamente') {
             this.showValidado = true;
           } else {
             this.messageNoValidado = response.message;
@@ -272,8 +343,7 @@ export class VisualizarArchivoComponent implements OnInit {
           }
 
           console.log('Archivo subido con éxito:', response);
-        },
-        error: (err) => {
+        }, error: (err) => {
           this.showLoadingModal = false;
           this.showErrorModal = true;
           console.error('Error al subir el archivo:', err);
@@ -299,15 +369,14 @@ export class VisualizarArchivoComponent implements OnInit {
       next: (response) => {
         this.messageNoValidado = response.message;
         this.showLoadingModal = false;
-        if(response.message == 'Todos los datos se han procesado y guardado correctamente.'){
+        if(response.message == 'Todos los datos se han procesado y guardado correctamente.') {
           this.showFinalModal = true;
         } else {
           this.showError = true;
         }
 
         console.log('Archivo subido con éxito:', response);
-      },
-      error: (err) => {
+      }, error: (err) => {
         this.showLoadingModal = false;
         this.showErrorModal = true;
         console.error('Error al subir el archivo:', err);
